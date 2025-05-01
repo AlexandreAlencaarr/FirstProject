e599e358f1233d8f514c0db629dc3afefaa65efb
Alexandre <alexandrealencar900@gmail.com>
2025-05-01 01:31:13 -0300

    subindo coisinhas

--- JS/script.js
+++ JS/script.js
@@ -11,50 +11,49 @@ img.src = "Imagens/dvd.webp";
 window.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
-  
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  
+
+  const img = new Image();
+  img.src = "Imagens/dvd.webp";
+
   let x = 100;
   let y = 100;
   let speedX = 3;
   let speedY = 3;
-  let radius = 30;
-  
+  const logoWidth = 100;
+  const logoHeight = 100;
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  window.addEventListener("resize", resizeCanvas);
+  resizeCanvas();
+
   img.onload = () => {
     function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, x, y, 100, 100); // x, y, width, height
+      ctx.drawImage(img, x, y, logoWidth, logoHeight);
+
       x += speedX;
       y += speedY;
-  
-      if (x + 60 > canvas.width || x < 0) speedX *= -1;
-      if (y + 60 > canvas.height || y < 0) speedY *= -1;
-  
+
+      if (x + logoWidth > canvas.width || x < 0) speedX *= -1;
+      if (y + logoHeight > canvas.height || y < 0) speedY *= -1;
+
       requestAnimationFrame(draw);
     }
-  
+
     draw();
   };
-
-  function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fillStyle = "red";
-    ctx.fill();
-  
-    x += speedX;
-    y += speedY;
-  
-    // bounce off edges
-    if (x + radius > canvas.width || x - radius < 0) speedX *= -1;
-    if (y + radius > canvas.height || y - radius < 0) speedY *= -1;
-  
-    requestAnimationFrame(draw);
-  }
-  
-  draw();
 });
 
+let logoWidth;
+let logoHeight;
 
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  logoWidth = canvas.width * 0.1;
+  logoHeight = logoWidth;
+}
